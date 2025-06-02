9638c360d4d3c509ea56b40837fc80c46cbdfa6b
Ryan <rt1824@ic.ac.uk>
2025-06-02 20:51:34 +0100

    Added IS_EQUAL function in utils.h file

---
+++ assemble/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H 
+#define UTILS_H 
+
+
+/**
+ * Determines if two strings are equal to one another
+ * 
+ * @return true on success and false on error. 
+ */
+#define IS_EQUAL(str1, str2) (strcmp(str1, str2) == 0)
+
+#endif 

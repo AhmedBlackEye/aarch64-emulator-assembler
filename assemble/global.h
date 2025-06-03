947bc080a1a1545c4a5b7a8285d7e61f5fea7542
Ahmed Elsayed <ae924@ac.ic.uk>
2025-06-03 20:58:36 +0100

    Adding global addr

--- assemble/global.h
+++ assemble/global.h
@@ -2,6 +2,7 @@
 #define GLOBAL_H 
 
 #include "string.h"
+#include "stdint.h"
 
 /**
  * Determines if two strings are equal to one another
@@ -9,5 +10,8 @@
  * @return true on success and false on error. 
  */
 #define STR_EQUAL(str1, str2) (strcmp(str1, str2) == 0)
+
+
+extern uint64_t curr_instr_addr;
 
 #endif 

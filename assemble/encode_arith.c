22cd11eead0f5670ba8deebbd1470813869ea5fa
Joshua Jones <jdj24@ic.ac.uk>
2025-06-03 15:34:30 +0100

    Refactored encode arithmetic functions to a new file called encode_arith and added header files to encode_arith, encode_dp_imm and encode_dp_reg

---
+++ assemble/encode_arith.c
@@ -0,0 +1,65 @@
+#include "encode_dp_imm.h"
+#include "encode_dp_imm.c"
+
+uint32_t encode_add(char **tokens, int size) {
+    if (tokens[2][0] == '#') {
+        return encode_arith_imm(tokens, size, 0);
+    }
+    else {
+        return encode_arith_reg(tokens, size, 0);
+    }
+}
+
+uint32_t encode_adds(char **tokens, int size) {
+    if (tokens[2][0] == '#') {
+        return encode_arith_imm(tokens, size, 1);
+    }
+    else {
+        return encode_arith_reg(tokens, size, 1);
+    }
+}
+
+uint32_t encode_sub(char **tokens, int size) {
+    if (tokens[2][0] == '#') {
+        return encode_arith_imm(tokens, size, 2);
+    }
+    else {
+        return encode_arith_reg(tokens, size, 2);
+    }
+}
+
+uint32_t encode_subs(char **tokens, int size) {
+    if (tokens[2][0] == '#') {
+        return encode_arith_imm(tokens, size, 3);
+    }
+    else {
+        return encode_arith_reg(tokens, size, 3);
+    }
+}
+
+uint32_t encode_cmp(char **tokens, int size) {
+    if (tokens[1][0] == '#') {
+        return encode_arith_imm(tokens, size, 3);
+    }
+    else {
+        return encode_arith_reg(tokens, size, 3);
+    }
+}
+
+uint32_t encode_cmn(char **tokens, int size) {
+    if (tokens[1][0] == '#') {
+        return encode_arith_imm(tokens, size, 1);
+    }
+    else {
+        return encode_arith_reg(tokens, size, 1);
+    }
+}
+
+uint32_t encode_neg(char **tokens, int size) {
+    if (tokens[1][0] == '#') {
+        return encode_arith_imm(tokens, size, 2);
+    }
+    else {
+        return encode_arith_reg(tokens, size, 2);
+    }
+}
---
+++ assemble/encode_dp_reg.h
@@ -0,0 +1,23 @@
+#ifndef ENCODE_DP_REG_H
+#define ENCODE_DP_REG_H
+
+#include <stdint.h>
+
+uint32_t encode_and  (char **tokens, int size);
+uint32_t encode_bic  (char **tokens, int size);
+uint32_t encode_orr  (char **tokens, int size);
+uint32_t encode_orn  (char **tokens, int size);
+uint32_t encode_eor  (char **tokens, int size);
+uint32_t encode_eon  (char **tokens, int size);
+uint32_t encode_ands (char **tokens, int size);
+uint32_t encode_bics (char **tokens, int size);
+uint32_t encode_tst  (char **tokens, int size);
+uint32_t encode_mvn  (char **tokens, int size);
+uint32_t encode_mov  (char **tokens, int size);
+uint32_t encode_madd(char **tokens, int size);
+uint32_t encode_msub(char **tokens, int size);
+uint32_t encode_mul (char **tokens, int size);
+uint32_t encode_mneg(char **tokens, int size);
+uint32_t encode_arith_reg(const char **tokens, int size, uint8_t opc);
+
+#endif

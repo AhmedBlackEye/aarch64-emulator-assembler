ea11400ab3be66fe917745da305614d5e7b56538
Joshua Jones <jdj24@ic.ac.uk>
2025-06-03 20:50:18 +0100

    Added correct shift amount to encode_multiply

--- assemble/encode_dp_reg.c
+++ assemble/encode_dp_reg.c
@@ -122,7 +122,7 @@ static uint32_t encode_multiply(char **tokens,
     uint8_t  x
 ) {
 
-    uint8_t ra = (size == 4) ? atoi(tokens[3] + 1) << 10 : 31;
+    uint8_t ra = (size == 4) ? atoi(tokens[3] + 1) : 31;
 
     uint8_t sf = (tokens[0][0] == 'x') ? 1 : 0;
 
@@ -131,7 +131,7 @@ static uint32_t encode_multiply(char **tokens,
     instr |= 0xD8 << 21;
     instr |= atoi(tokens[2] + 1) << 16;
     instr |= x << 15;
-    instr |= ra;
+    instr |= ra << 10;
     instr |= atoi(tokens[1] + 1) << 5;
     instr |= atoi(tokens[0] + 1);
     return instr;

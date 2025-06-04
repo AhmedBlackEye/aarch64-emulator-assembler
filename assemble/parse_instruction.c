7becdfd10c3c23753c4a4fe04027aeeb20c52c7d
Ahmed Elsayed <ae924@ac.ic.uk>
2025-06-04 17:55:21 +0100

    Fixing parsing_instr function to handle brackets

--- assemble/parse_instruction.c
+++ assemble/parse_instruction.c
@@ -2,16 +2,21 @@
 #include <string.h>
 #include <stdint.h>
 #include <ctype.h>
+
 #include "encoder_table.h"
 #include "debug.h"
 
 #define MAX_TOKENS 5
 
-static char *ltrim(char *s) {
-    while (isspace((unsigned char)*s)) s++;
+static char *ltrim(char *s)
+{
+    while (isspace((unsigned char)*s))
+        s++;
     return s;
 }
-uint32_t parse_instr(char *line) {
+
+uint32_t parse_instr(char *line)
+{
     char *tokens[MAX_TOKENS];
     int num_tokens = 0;
 
@@ -19,14 +24,38 @@ uint32_t parse_instr(char *line) {
     char *mnemonic = strtok_r(line, " \t", &saveptr);
     PANIC_IF(mnemonic == NULL, "parse_instr: no mnemonic read");
 
-    // Now tokenize on both commas and whitespace
     char *arg = strtok_r(NULL, " \t,", &saveptr);
-    while (arg != NULL && num_tokens < MAX_TOKENS) {
-        tokens[num_tokens++] = ltrim(arg);
+    while (arg != NULL && num_tokens < MAX_TOKENS)
+    {
+        if (arg[0] == '[')
+        {
+            char *start = arg;
+            char *end = strchr(start, ']');
+
+            while (!end)
+            {
+                char *next = strtok_r(NULL, " \t,", &saveptr);
+                if (!next)
+                {
+                    PANIC("parse_instr: unmatched [ in bracketed operand");
+                }
+
+                strcat(start, ",");
+                strcat(start, next);
+                end = strchr(start, ']');
+            }
+
+            tokens[num_tokens++] = ltrim(start);
+        }
+        else
+        {
+            tokens[num_tokens++] = ltrim(arg);
+        }
+
         arg = strtok_r(NULL, " \t,", &saveptr);
     }
 
-    PANIC_IF(num_tokens < 2, "parse_instr: insufficient operands for %s", mnemonic);
+    PANIC_IF(num_tokens < 1, "parse_instr: insufficient operands for %s", mnemonic);
 
     encode_function fn = lookup_encoder(mnemonic);
     return fn((const char **)tokens, num_tokens);

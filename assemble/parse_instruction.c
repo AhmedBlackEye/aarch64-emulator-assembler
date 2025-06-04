6e2084952f39c5abf27eabba028d03144758f118
Ryan <rt1824@ic.ac.uk>
2025-06-04 19:43:22 +0100

    fixed parse_instr to properly parse brackets

--- assemble/parse_instruction.c
+++ assemble/parse_instruction.c
@@ -7,6 +7,7 @@
 #include "debug.h"
 
 #define MAX_TOKENS 5
+#define BRACKET_BUFFER_SIZE 256
 
 static char *ltrim(char *s)
 {
@@ -29,23 +30,45 @@ uint32_t parse_instr(char *line)
     {
         if (arg[0] == '[')
         {
-            char *start = arg;
-            char *end = strchr(start, ']');
-
-            while (!end)
+            // Use a static buffer to safely build the bracketed expression
+            static char bracket_buffer[BRACKET_BUFFER_SIZE];
+            int buf_len = 0;
+            
+            // Copy the first part
+            int arg_len = strlen(arg);
+            PANIC_IF(arg_len >= BRACKET_BUFFER_SIZE, "Bracketed operand too long");
+            strcpy(bracket_buffer, arg);
+            buf_len = arg_len;
+            
+            // Check if this token already contains the closing bracket
+            if (!strchr(bracket_buffer, ']'))
             {
-                char *next = strtok_r(NULL, " \t,", &saveptr);
-                if (!next)
+                // Need to collect more tokens until we find the closing bracket
+                char *next;
+                while ((next = strtok_r(NULL, " \t,", &saveptr)) != NULL)
+                {
+                    // Add a comma and space, then the next token
+                    int next_len = strlen(next);
+                    PANIC_IF(buf_len + 2 + next_len >= BRACKET_BUFFER_SIZE, 
+                             "Bracketed operand too long");
+                    
+                    strcat(bracket_buffer, ", ");
+                    strcat(bracket_buffer, next);
+                    buf_len += 2 + next_len;
+                    
+                    if (strchr(next, ']'))
+                    {
+                        break; // Found the closing bracket
+                    }
+                }
+                
+                if (!strchr(bracket_buffer, ']'))
                 {
                     PANIC("parse_instr: unmatched [ in bracketed operand");
                 }
-
-                strcat(start, ",");
-                strcat(start, next);
-                end = strchr(start, ']');
             }
-
-            tokens[num_tokens++] = ltrim(start);
+            
+            tokens[num_tokens++] = ltrim(bracket_buffer);
         }
         else
         {

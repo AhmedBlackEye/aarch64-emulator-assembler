46d48ac92c0d9aaedc51c9db1e8566ebf73572f1
Ahmed Elsayed <ae924@ac.ic.uk>
2025-06-10 13:19:40 +0100

    Changing binary tree to a better implementation

--- common/bintree.c
+++ common/bintree.c
@@ -1,112 +1,167 @@
-#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
-#include "dynstr.h"
-#include "debug.h"
-#include "assert.h"
+#include "bintree.h"
+
+typedef void* GVal;
+typedef void (*PrintFn)(GVal val);
+typedef void (*FreeFn)(GVal val);
+typedef int (*CmpFn)(GVal v1, GVal v2);
 
 typedef struct Node *Node;
 struct Node {
+    GVal val;
     Node left;
-    Node right;    
-    DynStr dstr;
+    Node right;
 };
 
-Node make_node(char *str) {
-    Node new = malloc(sizeof(struct Node));
-    PANIC_IF(new == NULL, "Couldn't allocate memory");
-    new->left = NULL;
-    new->right = NULL;
-    new->dstr  = dynstr_create(str);
-    return new;
-}
-
-
-typedef struct BinTreeStr *BinTreeStr;
-struct BinTreeStr {
+struct BinTree {
     Node root;
     int size;
+    CmpFn cmp;
+    PrintFn pval;
+    FreeFn fval;
 };
 
-
-BinTreeStr make_bintreestr() {
-    BinTreeStr new = malloc(sizeof(struct BinTreeStr));
-    PANIC_IF(new == NULL, "Couldn't allocate memory");
-    new->root = NULL;
-    new->size = 0;
+Node create_node(GVal val) {
+    Node new = malloc(sizeof(struct Node));
+    assert(new != NULL);
+    new->val = val;
+    new->left = NULL;
+    new->right = NULL;
     return new;
 }
 
-static void destroy_bintreestr_helper(Node node) {
-    if(node == NULL) return;
-
-    destroy_bintreestr_helper(node->left);
-    destroy_bintreestr_helper(node->right);
-
-    dynstr_free(node->dstr);
-    free(node);
+void destroy_node(Node n) {
+    free(n);
 }
 
+BinTree create_tree(CmpFn cmp, PrintFn pval, FreeFn fval) {
+    BinTree new = malloc(sizeof(struct BinTree));
+    assert(new != NULL);
+    new->root = NULL;
+    new->size = 0;
+    new->cmp = cmp;
+    new->pval = pval;
+    new->fval = fval;
+    return new;
+}
 
-void destroy_bintreestr(BinTreeStr t) {
-    PANIC_IF(t == NULL, "Tree is NULL");
-    destroy_bintreestr_helper(t->root);
+static void destroy_tree_helper(Node n, FreeFn fval) {
+    if (n == NULL) return;
+    destroy_tree_helper(n->left, fval);
+    destroy_tree_helper(n->right, fval);
+    fval(n->val);
+    destroy_node(n);
+}
+
+void destroy_tree(BinTree t) {
+    assert(t != NULL);
+    destroy_tree_helper(t->root, t->fval);
     free(t);
 }
 
-static Node bintreestr_add_helper(Node currnode, char *str) {
-    if(currnode == NULL) return make_node(str);
-
-    int cmp = dynstr_cmp(str, currnode->dstr);
-    if(cmp <= 0) {
-        currnode->left  = bintreestr_add_helper(currnode->left, str);
+static void print_bintree_helper(Node n, PrintFn pf, int *first) {
+    if (n == NULL) return;
+    print_bintree_helper(n->left, pf, first);
+    
+    if (*first) {
+        *first = 0;
     } else {
-        currnode->right  = bintreestr_add_helper(currnode->right, str);
+        printf(", ");
+    }
+    pf(n->val);
+    
+    print_bintree_helper(n->right, pf, first);
+}
+
+void print_bintree(BinTree t) {
+    assert(t != NULL);
+    printf("[");
+    int first = 1;
+    print_bintree_helper(t->root, t->pval, &first);
+    printf("]\n");
+}
+
+static Node bintree_add_helper(Node curr_node, GVal v, BinTree t) {
+    if (curr_node == NULL) {
+        t->size++;
+        return create_node(v);
+    }
+    int res = t->cmp(v, curr_node->val);
+    if (res < 0) {
+        curr_node->left = bintree_add_helper(curr_node->left, v, t);
+    } else if (res > 0) {
+        curr_node->right = bintree_add_helper(curr_node->right, v, t);
     }
 
-    return currnode;
+    return curr_node;
 }
 
-void bintreestr_add(BinTreeStr t, char *str) {
-    PANIC_IF(t == NULL, "Tree is NULL");
-    t->root = bintreestr_add_helper(t->root, str);
-    t->size++;
+void bintree_add(BinTree t, GVal v) {
+    assert(t != NULL);
+    t->root = bintree_add_helper(t->root, v, t);
 }
 
-static void inorder(Node node, DynStr *arr, int *index) {
-    if (node == NULL) return;
-    inorder(node->left, arr, index);
-    arr[(*index)++] = node->dstr;
-    inorder(node->right, arr, index);
+static Node remove_max(Node root, Node *max_node) {
+    assert(root != NULL);
+    if (root->right == NULL) {
+        *max_node = root;
+        return root->left;
+    }
+    root->right = remove_max(root->right, max_node);
+    return root;
 }
 
-// Test function
-void test_bintreestr() {
-    BinTreeStr tree = make_bintreestr();
-    assert(tree != NULL);
-    assert(tree->size == 0);
+static Node bintree_remove_helper(Node curr_node, GVal target_val, BinTree t) {
+    if (curr_node == NULL) return NULL;
+    int res = t->cmp(target_val, curr_node->val);
+    if (res < 0) {
+        curr_node->left = bintree_remove_helper(curr_node->left, target_val, t);
+        return curr_node;
+    } else if (res > 0) {
+        curr_node->right = bintree_remove_helper(curr_node->right, target_val, t);
+        return curr_node;
+    }
 
-    bintreestr_add(tree, "banana");
-    bintreestr_add(tree, "apple");
-    bintreestr_add(tree, "cherry");
+    t->size--;
+    Node replacement = NULL;
+    if (curr_node->left == NULL) {
+        replacement = curr_node->right;
+    } else if (curr_node->right == NULL) {
+        replacement = curr_node->left;
+    } else {
+        Node max_node;
+        curr_node->left = remove_max(curr_node->left, &max_node);
+        max_node->left = curr_node->left;
+        max_node->right = curr_node->right;
+        replacement = max_node;
+    }
 
-    assert(tree->size == 3);
-
-    // Collect nodes in sorted order
-    DynStr arr[3];
-    int idx = 0;
-    inorder(tree->root, arr, &idx);
-
-    assert(strcmp(dynstr_data(arr[0]), "apple") == 0);
-    assert(strcmp(dynstr_data(arr[1]), "banana") == 0);
-    assert(strcmp(dynstr_data(arr[2]), "cherry") == 0);
-
-    printf("All bintreestr tests passed!\n");
-
-    destroy_bintreestr(tree);  // Formerly destory_bintreestr
+    t->fval(curr_node->val);
+    destroy_node(curr_node);
+    return replacement;
 }
 
-int main() {
-    test_bintreestr();
-    return 0;
+void bintree_remove(BinTree t, GVal v) {
+    assert(t != NULL);
+    t->root = bintree_remove_helper(t->root, v, t);
+}
+
+static GVal bintree_lookup_helper(Node curr_node, GVal v, BinTree t) {
+    if (curr_node == NULL) return NULL;
+    int res = t->cmp(v, curr_node->val);
+    if (res == 0) {
+        return curr_node->val;
+    } if (res < 0) {
+        return bintree_lookup_helper(curr_node->left, v, t);
+    } else {
+        return bintree_lookup_helper(curr_node->right, v, t);
+    }
+}
+
+GVal bintree_lookup(BinTree t, GVal v) {
+    assert(t != NULL);
+    return bintree_lookup_helper(t->root, v, t);
 }
--- common/bintree.h
+++ common/bintree.h
@@ -1,17 +1,106 @@
-#include <stdint.h>
+#ifndef BINTREE_H
+#define BINTREE_H
+
+#include <stdio.h>
 #include <stdlib.h>
 
-#include "dynstr.h"
+/**
+ * @brief Generic value type - can hold any data type
+ */
+typedef void* GVal;
 
-typedef struct Node *Node;
+/**
+ * @brief Function pointer type for printing values
+ * @param val The value to print
+ */
+typedef void (*PrintFn)(GVal val);
 
-extern Node make_node(char *str);
+/**
+ * @brief Function pointer type for freeing values
+ * @param val The value to free
+ */
+typedef void (*FreeFn)(GVal val);
 
-typedef struct BinTreeStr *BinTreeStr;
+/**
+ * @brief Function pointer type for comparing values
+ * @param v1 First value to compare
+ * @param v2 Second value to compare
+ * @return Negative if v1 < v2, 0 if v1 == v2, positive if v1 > v2
+ */
+typedef int (*CmpFn)(GVal v1, GVal v2);
 
+/**
+ * @brief Opaque pointer to a binary tree
+ */
+typedef struct BinTree *BinTree;
 
-extern BinTreeStr make_bintreestr();
+/**
+ * @brief Creates a new binary search tree
+ * @param cmp Comparison function for ordering elements
+ * @param pval Function for printing values
+ * @param fval Function for freeing values
+ * @return Pointer to the new tree, or NULL if allocation fails
+ */
+BinTree create_tree(CmpFn cmp, PrintFn pval, FreeFn fval);
 
-extern void destroy_bintreestr(BinTreeStr t);
+/**
+ * @brief Destroys a binary tree and frees all memory
+ * @param t The tree to destroy (must not be NULL)
+ */
+void destroy_tree(BinTree t);
 
-extern void bintreestr_add(BinTreeStr t, char *str);
+/**
+ * @brief Adds a value to the binary tree
+ * 
+ * If the value already exists in the tree (based on the comparison function),
+ * the duplicate is ignored and not added.
+ * 
+ * @param t The tree to add to (must not be NULL)
+ * @param v The value to add
+ */
+void bintree_add(BinTree t, GVal v);
+
+/**
+ * @brief Removes a value from the binary tree
+ * 
+ * If the value is not found, the tree remains unchanged.
+ * 
+ * @param t The tree to remove from (must not be NULL)
+ * @param v The value to remove
+ */
+void bintree_remove(BinTree t, GVal v);
+
+/**
+ * @brief Looks up a value in the binary tree
+ *  
+ * @param t The tree to add to (must not be NULL)
+ * @param v The value to add
+ * @return Pointer to the new value, or NULL if fails
+ */
+GVal bintree_lookup(BinTree t, GVal v);
+
+/**
+ * @brief Prints the tree in sorted order
+ * 
+ * Performs an in-order traversal and prints all values using the
+ * print function provided during tree creation.
+ * 
+ * @param t The tree to print (must not be NULL)
+ */
+void print_bintree(BinTree t);
+
+/**
+ * @brief Gets the number of elements in the tree
+ * @param t The tree to query (must not be NULL)
+ * @return The number of elements in the tree
+ */
+int bintree_size(BinTree t);
+
+/**
+ * @brief Checks if the tree is empty
+ * @param t The tree to check (must not be NULL)
+ * @return 1 if empty, 0 if not empty
+ */
+int bintree_is_empty(BinTree t);
+
+#endif /* BINTREE_H */
